9e65ddb906865f05737b88a89681f3d1b1e7bc6a
Jayson Olaguera <napsterbond@yahoo.com>
2020-12-19 23:09:26 +0800

    Initial commit

---
+++ src/Authentication.js
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import app from "./firebase.js";
+export const AuthContext = React.createContext();
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [pending, setPending] = useState(true);
+  useEffect(() => {
+    //if user is loged in
+    app.auth().onAuthStateChanged((user) => {
+      setCurrentUser(user)
+      setPending(false)
+    });
+  }, []);
+  if(pending){
+    return <>Please wait...</>
+  }
+  return (
+    <AuthContext.Provider
+      value={{
+        currentUser
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+
+
+
+export default AuthProvider;
